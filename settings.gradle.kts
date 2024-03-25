3111f6d4e7cd71b407dbc6ebd15b58ad0daec143
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-24 21:52:05 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Flixter part 2 and102-project4"
+include(":app")
